4af65dd708cd44eb473f17a8ba7d0882bdf9278c
luffy04 <anujjha041998@gmail.com>
2020-04-13 13:47:22 +0530

    Updated

--- server.js
+++ server.js
@@ -2,40 +2,40 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const fs=require('fs');
-const bcrypt=require('bcryptjs');
-const passport = require('passport');
-const passportLocal = require('passport-local').Strategy;
-const session = require('express-session');
+// const bcrypt=require('bcryptjs');
+// const passport = require('passport');
+// const passportLocal = require('passport-local').Strategy;
+// const session = require('express-session');
 const { exec } = require('child_process');
 const http=require('http');
 const server=http.Server(app);
 const socket=require('socket.io');
 const io=socket(server);
 const {c, cpp, node, python, java} = require('compile-run');
-const database=require('./database');
+// const database=require('./database');
 const saltRounds = 10;
 var names=[];
 var rooms=[];
 
-app.use(session({secret: 'I have a dog',resave:true,saveUninitialized:true}));
+// app.use(session({secret: 'I have a dog',resave:true,saveUninitialized:true}));
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.use(bodyParser.json({ limit: '10mb'}))
 
 app.use(bodyParser.json());
 
 
-app.post('/registers',(req,res)=>{
-    bcrypt.genSalt(saltRounds, function(err, salt) {
-        bcrypt.hash(req.body.password, salt, function(err, hash) {
-            database.signUp(req.body.username, req.body.email,hash, function(err,data) {
-                if(err) res.send(err)
-                else res.send(data);
-            })
-        });
-    });
-})
+// app.post('/registers',(req,res)=>{
+//     bcrypt.genSalt(saltRounds, function(err, salt) {
+//         bcrypt.hash(req.body.password, salt, function(err, hash) {
+//             database.signUp(req.body.username, req.body.email,hash, function(err,data) {
+//                 if(err) res.send(err)
+//                 else res.send(data);
+//             })
+//         });
+//     });
+// })
 
 app.get('/get',(req,res)=>{
     res.send(names);
@@ -102,48 +102,48 @@ app.post("/run", (req, res) => {
 });
 
 
-app.post('/loginsecure', passport.authenticate('local',{
-    successRedirect:'/success',
-    failureRedirect:'/failure'
-  }));
+// app.post('/loginsecure', passport.authenticate('local',{
+//     successRedirect:'/success',
+//     failureRedirect:'/failure'
+//   }));
   
-  passport.use(new passportLocal(
-      function(username, password, done) {
-          database.getUser(username, function(data) {
-            if(data==""){ 
-                return done(null,false,{message:"username is incorrect"});
-            }
-            else{
-                bcrypt.compare(password, data[0].password, function(err, res) {
-                    if(res){
-                        return done(null,data[0]);
-                    }else{
-                        return done(null, false, {message: 'password is incorrect'});
-                }
-              });
-            }
-          })
-      })
-  );
+//   passport.use(new passportLocal(
+//       function(username, password, done) {
+//           database.getUser(username, function(data) {
+//             if(data==""){ 
+//                 return done(null,false,{message:"username is incorrect"});
+//             }
+//             else{
+//                 bcrypt.compare(password, data[0].password, function(err, res) {
+//                     if(res){
+//                         return done(null,data[0]);
+//                     }else{
+//                         return done(null, false, {message: 'password is incorrect'});
+//                 }
+//               });
+//             }
+//           })
+//       })
+//   );
   
-  passport.serializeUser(function(id, done) {
-      return done(null, id);
-  });
+  // passport.serializeUser(function(id, done) {
+  //     return done(null, id);
+  // });
   
-  passport.deserializeUser(function(id, done) {
-      return done(null, id)
-  });
+  // passport.deserializeUser(function(id, done) {
+  //     return done(null, id)
+  // });
   
-  app.get('/success', function(req,res) {
-      v=req.user;
-      console.log(req.user);
-      res.send("success");
-  });
+  // app.get('/success', function(req,res) {
+  //     v=req.user;
+  //     console.log(req.user);
+  //     res.send("success");
+  // });
   
-  app.get('/failure', function(req,res) {
-      req.logout();
-      res.send('failure')
-  });
+  // app.get('/failure', function(req,res) {
+  //     req.logout();
+  //     res.send('failure')
+  // });
 
 io.sockets.on('connection',(socket)=>{
 
